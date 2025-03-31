5eaf822855f9c8b81cb26ea15b67d638b16d51f6
Ethan Chen <echen@lcs.on.ca>
2025-03-31 14:48:50 -0400

    Initial Commit

---
+++ QuoteApp/QuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppApp.swift
+//  QuoteApp
+//
+//  Created by Pak Ching Ethan Chen on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct QuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
