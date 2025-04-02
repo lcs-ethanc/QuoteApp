7d873937b20e1fc63ac6bf073501c17e5487ec00
Ethan Chen <echen@lcs.on.ca>
2025-04-02 19:18:40 -0400

    started fetchQuote function, check if valid URL

--- QuoteApp/ViewModel/QuoteViewModel.swift
+++ QuoteApp/ViewModel/QuoteViewModel.swift
@@ -14,7 +14,21 @@ class QuoteViewModel {
     //Most recent quote
     var currentQuote: Quote?
     
+    //MARK: Initialiser
     init(currentQuote: Quote? = nil) {
         self.currentQuote = currentQuote
     }
+    
+    //MARK: Function
+    
+    func fetchQuote() async{
+        
+        //Create URL from provided address (check if valid URL)
+        let endpoint = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en"
+        guard let url = URL(string: endpoint) else{
+            print("Invalid address for JSON endpoint")
+            return
+        }
+    }
+    
 }
