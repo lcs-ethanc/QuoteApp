9b2ec4780a7c351b25fdcbefc50f8c724bf19f33
Ethan Chen <echen@lcs.on.ca>
2025-04-02 19:27:03 -0400

    set up quote view

---
+++ QuoteApp/View/QuoteView.swift
@@ -0,0 +1,16 @@
+//
+//  QuoteView.swift
+//  QuoteApp
+//
+//  Created by Pak Ching Ethan Chen on 2025-04-02.
+//
+
+import SwiftUI
+
+struct QuoteView: View {
+    // MARK: Stored Properties
+    // MARK: Computed Properties
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
