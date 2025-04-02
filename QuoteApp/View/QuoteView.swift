c4834190658a4ad98d4aa15b3ae29a84d2969786
Ethan Chen <echen@lcs.on.ca>
2025-04-02 19:30:18 -0400

    set up stored properties for QuoteView

--- QuoteApp/View/QuoteView.swift
+++ QuoteApp/View/QuoteView.swift
@@ -9,6 +9,16 @@ import SwiftUI
 
 struct QuoteView: View {
     // MARK: Stored Properties
+    
+    //View Model of default joke
+    @State var viewModel = QuoteViewModel(currentQuote: exampleQuote)
+    
+    // Controls button visibility
+    @State var buttonOpacity = 0.0
+    
+    // Timer for button reveal
+    @State var buttonTimer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    
     // MARK: Computed Properties
     var body: some View {
         /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
